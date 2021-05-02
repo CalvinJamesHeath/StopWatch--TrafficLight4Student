55ac2b352da6d411ad8d75af2f2558709137f2b2
Calvin James Heath <calvinjamesheath14@hotmail.com>
2021-05-02 20:35:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Title } from './styled';
+import { Wrapper } from './styled';
+import { Container } from './styled';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [clicked, setClicked] = useState(false);
+
+	const encender = () => {
+		setClicked(!clicked);
+	};
+	const Circle = styled.div`
+		border-radius: 50%;
+		width: 500px;
+		height: 500px;
+		:hover {
+			cursor: pointer;
+			border: 3px solid #2ecc71;
+		}
+		background: ${clicked ? '#66ff00;' : '#16a085'};
+		margin: auto;
+		position: relative;
+		top: 40px;
+	`;
+	return (
+		<div className='App'>
+			<Wrapper>
+				<Title>Haz Click Para Encender!</Title>
+				<Container>
+					<Circle onClick={encender} />
+				</Container>
+			</Wrapper>
+			{/* <div className='contenedor'>
+				<div class='circleBase type1'></div>
+			</div> */}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+	font-size: 2.5em;
+	text-align: center;
+	color: #000;
+	text-decoration: underline;
+`;
+
+// background-color: lightseagreen;
+export const Wrapper = styled.section`
+	color: white;
+	position: relative;
+	text-align: center;
+`;
+export const Container = styled.div`
+	width: 600px;
+	margin: 3rem auto;
+	height: 600px;
+	background-color: black;
+`;
